4ea41c706ecc102c144ba24c7efffdde556a44e0
daorichard <98173508+daorichard@users.noreply.github.com>
2023-06-01 22:36:58 -0400

    add player and computer paddle functionality

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 import Ball from "./Ball.js";
-
+import Paddle from "./Paddle.js";
 const ball = new Ball(document.getElementById("ball"));
+const playerPaddle = new Paddle(document.getElementById("player-paddle"));
+const computerPaddle = new Paddle(document.getElementById("computer-paddle"));
 
 let lastTime;
 function update(time) {
   if (lastTime) {
     const delta = time - lastTime;
     // update code
-    ball.update(delta);
+    // ball.update(delta);
   }
 
   lastTime = time;
   window.requestAnimationFrame(update);
 }
 
+// mouse movement with player paddle
+document.addEventListener("mousemove", (e) => {
+  playerPaddle.position = e.y;
+});
 window.requestAnimationFrame(update);
