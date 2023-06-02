e94f82d76d29859edde90359ec5e4d783f471ef6
daorichard <98173508+daorichard@users.noreply.github.com>
2023-06-01 22:33:09 -0400

    add ball velocity bouncing functionality

--- Ball.js
+++ Ball.js
@@ -1,6 +1,9 @@
+const INITIAL_VELOCITY = 0.025;
+const VELOCITY_INCREASE = 0.00001;
 export default class Ball {
   constructor(ballElem) {
     this.ballElem = ballElem;
+    this.reset();
   }
   get x() {
     return parseFloat(getComputedStyle(this.ballElem).getPropertyValue("--x"));
@@ -10,7 +13,48 @@ export default class Ball {
     this.ballElem.style.setProperty("--x", value);
   }
 
-  update(delta) {
-    this.x = 5;
+  get y() {
+    return parseFloat(getComputedStyle(this.ballElem).getPropertyValue("--y"));
   }
+
+  set y(value) {
+    this.ballElem.style.setProperty("--y", value);
+  }
+
+  rect() {
+    return this.ballElem.getBoundingClientRect();
+  }
+  // functionality for ball movement, vector x-y
+  reset() {
+    this.x = 50;
+    this.y = 50;
+    this.direction = { x: 0 };
+    while (
+      Math.abs(this.direction.x <= 0.2) ||
+      Math.abs(this.direction.x) >= 0.9
+    ) {
+      const heading = randomNumberBetween(0, 2 * Math.PI);
+      this.direction = { x: Math.cos(heading), y: Math.sin(heading) };
+    }
+    this.velocity = INITIAL_VELOCITY;
+  }
+  update(delta) {
+    this.x += this.direction.x * this.velocity * delta;
+    this.y += this.direction.y * this.velocity * delta;
+    this.velocity += VELOCITY_INCREASE * delta;
+    const rect = this.rect();
+
+    // bouncing off walls functionality
+    if (rect.bottom >= window.innerHeight || rect.top <= 0) {
+      this.direction.y *= -1;
+    }
+
+    if (rect.left >= window.innerWidth || rect.right <= 0) {
+      this.direction.x *= -1;
+    }
+  }
+}
+
+function randomNumberBetween(min, max) {
+  return Math.random() * (max - min) + min;
 }
